46b3ba417b1958d59dd5d73f211bcfa76bcaaeff
keshabbhatt <keshabrajbhatt12345@gmail.com>
2024-06-08 12:09:17 +0545

    ki

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
-import Banner from './Banner';
-import Footer from './Footer';
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div>
       <Navbar />
-      <Banner />
       {/* Add other components and content here */}
-      <div className="flex-grow"></div>
-      <Footer />
     </div>
   );
 };
